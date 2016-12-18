f6d8abb829df071fff87adff96daa0db17529c3e
root <kfronczak@gmail.com>
2016-12-18 10:34:18 -0500

    Moved logging function to a class

---
+++ app/logger.py
@@ -0,0 +1,21 @@
+'''
+Custom logging class (really dumb)
+''' 
+import os
+import time
+ 
+class Logger(object):
+  def __init__(self, filename="log.txt"):
+    self.filename = filename
+    if os.path.isfile(self.filename):
+      os.remove(self.filename)
+      
+    self.write("Starting on "+time.strftime("%Y-%m-%d %H:%M:%S"))
+    
+  def write(self, message, debug=False):
+    self.log = open(self.filename, "a")
+    self.log.write(message)
+    self.log.write('\n')
+    self.log.close()
+    if debug:
+      print(message)
--- reinhart_not_hardt.py
+++ reinhart_not_hardt.py
@@ -7,15 +7,17 @@ Desc  : How hard is it to spell 'Reinhart'?  Apparently really fucking difficult
 '''
 
 import praw
-import time
-import os
 import sys
+import os
+import time
+from app import logger
+
+LOGGER  = logger.Logger("reinhart_not_hardt.log")
 
 def main():
-  sys.stdout = Logger("bot.log")
   reddit     = praw.Reddit('reinhart_not_hardt')
   subreddit  = reddit.subreddit("sabres")
-  
+   
   if not os.path.isfile("already_replied.txt"): 
     already_done = []
   else:
@@ -31,7 +33,7 @@ def main():
     
     # Run every 5 minutes
     save_post_data(already_done)
-    print("Sleeping")
+    LOGGER.write("Sleeping")
     time.sleep(300)
     
 
@@ -53,25 +55,25 @@ def fix_post_errors(submission, already_done):
     
     # Check if I've responded at all    
     if comment.author == 'reinhart_not_hardt' and submission.id not in already_done:
-      print('Already commented on: '+submission.title)
+      LOGGER.write('Already commented on: '+submission.title)
       already_done.append(submission.id)
     for reply in comment.replies:
       if reply.author == 'reinhart_not_hardt' and comment.link_id not in already_done:
-        print('Already Replied to: '+comment.body)
+        LOGGER.write('Already Replied to: '+comment.body)
         already_done.append(comment.link_id)
       
     lc_comment = comment.body.lower()
     if 'reinhardt' in lc_comment and comment.link_id not in already_done:
       comment.reply("#")
-      print("Replying to Comment: "+lc_comment)
+      LOGGER.write("Replying to Comment: "+lc_comment)
       already_done.append(comment.link_id)
       
   # Check title
   if submission.id not in already_done:
     if 'reinhardt' in lc_title or 'reinhardt' in lc_text:
       submission.reply('#')
-      print('Replying to: '+submission.title)
-      print(submission.id)
+      LOGGER.write('Replying to: '+submission.title)
+      LOGGER.write(submission.id)
       already_done.append(submission.id)
     
 def save_post_data(data):
@@ -79,20 +81,6 @@ def save_post_data(data):
   for id in data:
     f.write(id+'\n')
   f.close()
-
-'''
-Class to redirect stdout
-See comment from Gunslinger
-http://stackoverflow.com/questions/4675728/redirect-stdout-to-a-file-in-python
-'''  
-class Logger(object):
-  def __init__(self, filename="log.txt"):
-    self.terminal = sys.stdout
-    self.log = open(filename, "a")
-    
-  def write(self, message):
-    self.terminal.write(message)
-    self.log.write(message)
     
 if __name__ == '__main__':
   main()
